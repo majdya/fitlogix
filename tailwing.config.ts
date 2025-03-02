7820e3a4030333cfcd499de4c230d86daab373f3
majdya <majd.ya.1995@gmail.com>
2025-03-02 08:23:30 +0200

    tailwindcss setup

---
+++ tailwing.config.ts
@@ -0,0 +1,9 @@
+// filepath: /home/majd/github/fitlogix/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
