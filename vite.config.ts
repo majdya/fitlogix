0a6c1237cadc976514a093cfcac9b074ef08283d
majdya <majd.ya.1995@gmail.com>
2025-03-05 23:01:05 +0200

    fixed reletave pathes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    react(), // React plugin should be first
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
