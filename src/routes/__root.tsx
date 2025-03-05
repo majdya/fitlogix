21d4859195c9ec8f948172375b278c680928c143
majdya <majd.ya.1995@gmail.com>
2025-03-05 21:04:41 +0200

    implement sidebar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,27 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
+
+import { SidebarProvider, SidebarTrigger } from "../components/ui/sidebar"
+import { AppSidebar } from "../components/app-sidebar"
+
+
+
+
+
+
 export const Route = createRootRoute({
-    component: () => (
+    component: (/*{ children }: { children: React.ReactNode }*/) => (
         <>
-            <div className="p-2 flex gap-2">
-                <Link to="/" className="[&.active]:font-bold">
-                    Home
-                </Link>{' '}
-                <Link to="/about" className="[&.active]:font-bold">
-                    About
-                </Link>
-            </div>
-            <hr />
-            <Outlet />
-            <TanStackRouterDevtools />
+            <SidebarProvider>
+                <AppSidebar />
+                <main>
+                    <SidebarTrigger />
+                    {/* {children} */}
+                    <Outlet />
+                    <TanStackRouterDevtools />
+                </main>
+            </SidebarProvider>
         </>
     ),
 })
